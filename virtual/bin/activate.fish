e53c670aad962594f8b7c7894a2ae3ae72e7e471
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-02 16:34:39 +0300

    update virtual env

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa/Desktop/Python_Files/django-files/Moringa-Tribune/django_project/virtual"
+set -gx VIRTUAL_ENV "/home/moringa/Desktop/Python_Files/django-files/my_insta/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
