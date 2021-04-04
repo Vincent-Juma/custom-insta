0bf50a99f21cd3529ab5c3051ea006339dfecbb8
Vincent-Juma <vincentjuma64@gmail.com>
2021-04-04 13:53:35 +0300

    heroku deployment

--- django_project/settings.py
+++ django_project/settings.py
@@ -30,9 +30,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = True
 
 ALLOWED_HOSTS = ['myinstaprojectapp.herokuapp.com']
 
